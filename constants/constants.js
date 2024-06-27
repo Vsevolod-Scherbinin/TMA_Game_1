dc5a49e5bc0df73bf451ba1da7932b99daa07cf0
Vsevolod Scherbinin <vsevolod.o.sh@yandex.ru>
2024-06-27 19:35:24 +0300

    TMA_Game_1 - v.0.1.2 fix: testField + navigation

--- constants/constants.js
+++ constants/constants.js
@@ -3,6 +3,11 @@ const nameField = document.querySelector('.mainScreen__name');
 const testField = document.querySelector('.mainScreen__test');
 const btnMain = document.querySelector('.mainScreen__button');
 const scoreField = document.querySelector('.mainScreen__score');
+const navSection = document.querySelector('.navigation');
+const btnMainScreen = document.querySelector('.navigation__button_type_mainScreen');
+const btnBoosters = document.querySelector('.navigation__button_type_boosters');
+const btnTasks = document.querySelector('.navigation__button_type_tasks');
+const btnAchievements = document.querySelector('.navigation__button_type_achievement');
 
 let score = 0;  // make condition of loaded data
 let delta = 1;  // make condition of loaded data
--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,22 @@ function clickCounter () {
 
 btnMain.addEventListener('click', clickCounter);
 
+btnMainScreen.addEventListener('click', () => {
+  testField.textContent = btnMainScreen.querySelector('.navigation__btnName').textContent;
+});
+
+btnBoosters.addEventListener('click', () => {
+  testField.textContent = btnBoosters.querySelector('.navigation__btnName').textContent;
+});
+
+btnTasks.addEventListener('click', () => {
+  testField.textContent = btnTasks.querySelector('.navigation__btnName').textContent;
+});
+
+btnAchievements.addEventListener('click', () => {
+  testField.textContent = btnAchievements.querySelector('.navigation__btnName').textContent;
+});
+// testField.textContent = ``;
 nameField.textContent = TMA.initDataUnsafe.user.first_name;
 testField.textContent = TMA.initData;
-console.log(TMA);
+// console.log(TMA);
